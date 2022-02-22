7c23cf07b35a46302483708b5dfc1108cb0173aa
AsterixxxGallier <48604279+AsterixxxGallier@users.noreply.github.com>
2022-02-22 21:46:27 +0100

    mi pona mute a e lipu ilo nanpa ni a

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
-const sitelenPona: {[nimi: string]: Image} = {
+const sitelenPona: { [nimi: string]: Image } = {
     o: images.createImage(`
         . . . . .
-        . . # . .
-        . # . # .
-        . . # . .
+        . # . . .
+        # . # . .
+        . # . . .
         . . . . .
     `),
     pona: images.createImage(`
@@ -15,74 +15,61 @@ const sitelenPona: {[nimi: string]: Image} = {
     `)
 }
 
-const openEnPini: {[nimi: string]: [number, number]} = {
-    o: [1, 4],
-    pona: [0, 5]
+const suliNimi: { [nimi: string]: number } = {
 }
 
+Object.keys(sitelenPona).forEach(nimi => {
+    const sitelen = sitelenPona[nimi]
+    let suli = 0
+    for (let x = 0; x < 5; x++) {
+        let empty = true
+        for (let y = 0; y < 5; y++)
+            if (sitelen.pixel(x, y))
+                empty = false
+        if (!empty)
+            suli = x + 1
+    }
+    suliNimi[nimi] = suli
+})
+
 let lipu: string[] = []
+let lonNimi: number[] = []
+let suliLipu: number = 0
+
+function paliSin(nimi: string) {
+    lipu.push(nimi)
+    lonNimi.push(suliLipu)
+    suliLipu += suliNimi[nimi] + 1
+}
 
 input.onButtonPressed(Button.A, function () {
-    lipu.push("o")
+    paliSin("o")
 })
 
 input.onButtonPressed(Button.B, function () {
-    lipu.push("pona")
+    paliSin("pona")
 })
 
-let lon = 0
-let nanpaNi = 0
-let nanpaKama = 1
-let nimiNi: string | null = null
-let nimiKama: string | null = null
-let sitelenNi: Image | null = null
-let sitelenKama: Image | null = null
-let openNi: number | null = null
-let openKama: number | null = null
-let piniNi: number | null = null
-let piniKama: number | null = null
-
+let lon = -4
 basic.forever(function () {
     if (lipu.length) {
-        if (!nimiNi || !sitelenNi) {
-            nimiNi = lipu[nanpaNi]
-            sitelenNi = sitelenPona[nimiNi];
-            [openNi, piniNi] = openEnPini[nimiNi]
-        }
         if (lipu.length > 1) {
-            if (!nimiKama || !sitelenKama) {
-                nimiKama = lipu[nanpaKama]
-                sitelenKama = sitelenPona[nimiKama];
-                [openKama, piniKama] = openEnPini[nimiKama]
-            }
-            for (let x = 0; x < 5; x++) {
-                for (let y = 0; y < 5; y++) {
-                    led.unplot(x, y)
-                    if (sitelenNi.pixel(x + lon + openNi + (piniNi - 4), y))
-                        led.plot(x, y)
-                    if (sitelenKama.pixel(x + lon + openKama - piniNi, y))
-                        led.plot(x, y)
-                }
-            }
+            const sitelenSin = images.createImage("")
+            lipu.forEach((nimi, nanpa) => {
+                const sitelen = sitelenPona[nimi]
+                for (let x = 0; x < 5; x++)
+                    for (let y = 0; y < 5; y++)
+                        if (sitelen.pixel(x - lonNimi[nanpa] + lon, y))
+                            sitelenSin.setPixel(x, y, true)
+            })
+            sitelenSin.showImage(0, 0)
             lon++
-            if (lon >= piniNi - openKama + 1) {
-                lon = 0
-                nanpaNi++
-                nanpaKama++
-                if (nanpaNi >= lipu.length)
-                    nanpaNi = 0
-                if (nanpaKama >= lipu.length)
-                    nanpaKama = 0
-                nimiNi = lipu[nanpaNi]
-                sitelenNi = sitelenPona[nimiNi];
-                [openNi, piniNi] = openEnPini[nimiNi]
-                nimiKama = lipu[nanpaKama]
-                sitelenKama = sitelenPona[nimiKama];
-                [openKama, piniKama] = openEnPini[nimiKama]
+            if (lon >= suliLipu) {
+                lon = -4
             }
         } else {
-            sitelenNi.showImage(0, 0)
+            sitelenPona[lipu[0]].showImage(0, 0)
         }
     }
-    pause(400)
+    pause(100)
 })
